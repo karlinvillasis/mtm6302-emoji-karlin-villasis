7b63e014d717d32f60ab97e87068541450a7e7cd
vill0420 <113049059+karlinvillasis@users.noreply.github.com>
2023-11-22 09:32:34 -0500

    completes assignment

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ for(let i=50;i<62;i++) {
     <span>&#1294${i};</span><br>
     <code>1294${i}</code>
   </p>`
+  console.log(content)
   emoji.push(content)
 }
+console.log(emoji)
 
 container.innerHTML = emoji.join("")
 
